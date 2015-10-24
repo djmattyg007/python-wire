a88477b13bcbd8a325c0404ec91135a927d29a1b
Matthew Gamble <git@matthewgamble.net>
2015-10-24 23:09:32 +1100

    Add setup.py file to allow installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="wire",
+      version="1.0.0",
+      description="A convenience wrapper around the built-in SQLite3 module in python",
+      author="Rusby Panchal, Matthew Gamble",
+      author_email="git@matthewgamble.net",
+      url="https://github.com/djmattyg007/python-wire",
+      download_url="https://github.com/djmattyg007/python-wire/archive/1.0.0.zip",
+      packages=find_packages(),
+      package_data={},
+      data_files=[],
+      license="GPLv3",
+      install_requires=[]
+      )
+
